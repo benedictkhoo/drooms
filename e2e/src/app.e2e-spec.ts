e4713b348144aaaa2b13f413674356f45fcf1c6d
Benedict Khoo <3623419+benedictkhoo@users.noreply.github.com>
2020-01-28 18:12:20 +0800

    refactor: add UserModule

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to drooms!');
+    expect(page.getParagraphText()).toEqual('Drooms\' Exercise');
   });
 });
---
+++ src/app/user/user.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { UserRoutingModule } from './user-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    UserRoutingModule
+  ],
+  declarations: []
+})
+export class UserModule { }
